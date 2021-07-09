ef40ef483ec1f34ac594d81b1afafc6dbc27ba5b
an2arch <kartovcev01@mail.ru>
2021-07-09 19:23:22 +0300

    Initialization of the project

---
+++ AutoShop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+using AutoShop.Screens.Auth;
+using AutoShop.Screens.MainForm;
+
+namespace AutoShop
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new Auth());
+
+            Storage.Storage storage = Storage.Storage.getStorage();
+
+            if (storage.getState().currentUser != null)
+            {
+                Application.Run(new MainForm());
+            }
+        }
+    }
+}
